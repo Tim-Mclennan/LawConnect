6e14429edd349f6c496fe6a4f266fc3bfeaeac01
Tim McLennan <102727510+Tim-Mclennan@users.noreply.github.com>
2023-01-22 16:37:49 +1100

    first committ with draft for LawConnect

---
+++ main.js
@@ -0,0 +1,16 @@
+const menu = document.getElementById("navBtn");
+const nav = document.getElementById("navBarLinks");
+const close = document.getElementById("menuClose");
+
+/*If anyone clicks on the element bar, it will activate this bar variable event*/
+if (menu) { 
+        menu.addEventListener('click', () => {  /*If click is used, arrow function will trigger nav to add the '.active' class*/
+        nav.classList.add('active');
+     })
+}
+
+if (close) { /*If anyone clicks on X at the mobile screen menu, it will remove the '.active' class */
+        close.addEventListener('click', () => {  
+        nav.classList.remove('active');
+     })
+    }
